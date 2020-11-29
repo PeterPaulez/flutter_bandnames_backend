c4de499ef2cc8d4180de3c5576fd1517e7347808
Pedro Pablo Aparicio <pedroapariciocristobal@gmail.com>
2020-11-29 20:22:26 +0100

    NEW: Controlamos todo lo que sale en la app desde el Backend y actualizamos el socket en tiempo real.

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,8 +1,17 @@
 const {io} = require('../index'); // Importar el Exportanción de INDEX
+const Band = require('../models/band');
+const Bands = require('../models/bands');
+const bands = new Bands();
+bands.addBand(new Band('Queen'));
+bands.addBand(new Band('Héroes'));
+bands.addBand(new Band('Depeche Mode'));
+bands.addBand(new Band('Beatles'));
+console.log(bands);
 
 // Mensajes de Sockets
 io.on('connection', client => {
     console.log('Cliente conectado');
+    client.emit('active-bands', bands.getBands());
 
     client.on('disconnect', () => { 
         console.log('Cliente desconectado');
@@ -14,5 +23,41 @@ io.on('connection', client => {
         
         // Se lo envio a TODOS los clientes
         io.emit('mensaje', {admin: 'Nuevo mensaje'});
-    })
+    });
+
+
+    // Igual que el EMIT: mensaje
+    client.on('sent-mensaje', (payload) => {
+        console.log('mensaje', payload);
+        
+        // Se lo envio a TODOS los clientes
+        //io.emit('sent-mensaje', payload);
+
+        // Se lo envio a todo el mundo menos al que lo manda
+        client.broadcast.emit('sent-mensaje', payload);
+    });
+
+
+    // Igual que el EMIT: mensaje
+    client.on('vote-band', (payload) => {
+        console.log('Vote:', payload);
+        bands.voteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+    });
+
+
+    // Igual que el EMIT: mensaje
+    client.on('add-band', (payload) => {
+        const newBand = new Band(payload.name);
+        console.log('Add:', newBand);
+        bands.addBand(newBand);
+        io.emit('active-bands', bands.getBands());
+    });
+
+    // Igual que el EMIT: mensaje
+    client.on('del-band', (payload) => {
+        console.log('Delete:', payload);
+        bands.delBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+    });
 });
